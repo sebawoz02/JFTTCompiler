2630cff9874c20f0f767cb157404f7ae9756b759
sebawoz02 <sebawoz2002@gmail.com>
2023-12-29 16:27:31 +0100

    .

--- parser_error_functions.py
+++ parser_error_functions.py
@@ -1,3 +1,10 @@
+def _exit(par, string):
+    par.cg.close()
+    print(string)
+    print("Compilation failed!")
+    exit(1)
+
+
 def EXCEPTION_WRAPPER(par, ex, func, *args):
     try:
         return func(*args)
@@ -10,10 +17,7 @@ def EXCEPTION_WRAPPER(par, ex, func, *args):
 def check_if_set(par, pp, p, mode='error'):
     def error_msg():
         if mode == 'error':
-            par.cg.close()
-            print(f"\033[91mUse of unset variable '{pp.identifier}' in line {p.lineno}!\033[0m")
-            print("Compilation failed!")
-            exit(1)
+            _exit(par, f"\033[91mUse of unset variable '{pp.identifier}' in line {p.lineno}!\033[0m")
         else:
             print(f"WARNING! Use of unset variable in {mode}. line={p.lineno}.")
 
@@ -31,10 +35,7 @@ def check_if_set(par, pp, p, mode='error'):
     elif pp.v_type == "PIDENTIFIER":
         if not par.allocator.is_set(pp):
             if mode == 'error':
-                par.cg.close()
-                print(f"\033[91mUse of unset variable '{pp.identifier}' in line {p.lineno}!\033[0m")
-                print("Compilation failed!")
-                exit(1)
+                _exit(par, f"\033[91mUse of unset variable '{pp.identifier}' in line {p.lineno}!\033[0m")
             else:
                 print(f"WARNING! Use of unset variable in {mode}. line={p.lineno}.")
     elif pp.v_type == "AKU":
@@ -46,48 +47,30 @@ def check_if_not_array(par, pp, p):
     if par.pg.definition:
         info = par.EXCEPTION_WRAPPER(NameError, par.pg.get_param_info, pp)
         if info['type'] == 'T':
-            par.cg.close()
-            print(f"\033[91mProhibited use of array type variable '{pp}' in line {p.lineno}!\033[0m")
-            print("Compilation failed!")
-            exit(1)
+            _exit(par, f"\033[91mProhibited use of array type variable '{pp}' in line {p.lineno}!\033[0m")
     else:
         info = par.EXCEPTION_WRAPPER(NameError, par.allocator.get_type, pp)
         if info == 'T':
-            par.cg.close()
-            print(f"\033[91mProhibited use of array type variable '{pp}' in line {p.lineno}!\033[0m")
-            print("Compilation failed!")
-            exit(1)
+            _exit(par, f"\033[91mProhibited use of array type variable '{pp}' in line {p.lineno}!\033[0m")
 
 
 def check_if_array(par, pp, p):
     if par.pg.definition:
         info = par.EXCEPTION_WRAPPER(NameError, par.pg.get_param_info, pp)
         if info['type'] == '1':
-            par.cg.close()
-            print(f"\033[91mProhibited use of int type variable '{pp}' in line {p.lineno}!\033[0m")
-            print("Compilation failed!")
-            exit(1)
+            _exit(par, f"\033[91mProhibited use of int type variable '{pp}' in line {p.lineno}!\033[0m")
     else:
         info = par.EXCEPTION_WRAPPER(NameError, par.allocator.get_type, pp)
         if info == '1':
-            par.cg.close()
-            print(f"\033[91mProhibited use of int type variable '{pp}' in line {p.lineno}!\033[0m")
-            print("Compilation failed!")
-            exit(1)
+            _exit(par, f"\033[91mProhibited use of int type variable '{pp}' in line {p.lineno}!\033[0m")
 
 
 def check_if_index_in_range(par, pp, num, p):
     if par.pg.definition:
         info = par.EXCEPTION_WRAPPER(NameError, par.pg.get_param_info, pp)
         if info['size'] <= num and info['size'] != -1:
-            par.cg.close()
-            print(f"\033[91mArray '{pp}' index {num} out of range! Line {p.lineno}.\033[0m")
-            print("Compilation failed!")
-            exit(1)
+            _exit(par, f"\033[91mArray '{pp}' index {num} out of range! Line {p.lineno}.\033[0m")
     else:
         info = par.EXCEPTION_WRAPPER(NameError, par.allocator.get_info, pp)
         if info['size'] <= num:
-            par.cg.close()
-            print(f"\033[91mArray '{pp}' index {num} out of range! Line {p.lineno}.\033[0m")
-            print("Compilation failed!")
-            exit(1)
+            _exit(par, f"\033[91mArray '{pp}' index {num} out of range! Line {p.lineno}.\033[0m")
